34c6776f9e0d422057a3b63aa4da5e598ddabc0a
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2026-03-12 16:34:17 +0800

    Update security_helper.php

--- app/Helpers/security_helper.php
+++ app/Helpers/security_helper.php
@@ -30,28 +30,85 @@ if (!function_exists('sanitize_input')) {
 
 if (!function_exists('sanitize_html_content')) {
     /**
-     * Sanitize HTML content while preserving safe tags
+     * Sanitize HTML content while preserving safe tags and CKEditor formatting
      *
      * @param string $html
      * @return string
      */
     function sanitize_html_content($html)
     {
-        // Define allowed tags
-        $allowedTags = '<p><br><strong><b><em><i><u><h1><h2><h3><h4><h5><h6><ul><ol><li><a><img><table><tr><td><th><thead><tbody><div><span>';
+        // Define allowed tags (expanded for CKEditor)
+        $allowedTags = '<p><br><strong><b><em><i><u><h1><h2><h3><h4><h5><h6><ul><ol><li><a><img><table><tr><td><th><thead><tbody><tfoot><div><span><figure><figcaption><blockquote><pre><code><hr><mark><del><ins><sub><sup><small><big><center><font><s><strike>';
 
         // Strip dangerous tags first
         $html = strip_tags($html, $allowedTags);
 
         // Remove potentially dangerous attributes and javascript
         $html = preg_replace('/javascript:/i', '', $html);
+        $html = preg_replace('/vbscript:/i', '', $html);
         $html = preg_replace('/on\w+\s*=/i', '', $html); // Remove onload, onclick, etc.
-        $html = preg_replace('/style\s*=/i', '', $html); // Remove style attributes
-        $html = preg_replace('/data-\w+\s*=/i', '', $html); // Remove data attributes
+
+        // Allow class and id attributes (needed for CKEditor)
+        // But remove data- attributes that could be dangerous
+        $html = preg_replace('/data-\w+\s*=\s*["\'][^"\']*["\']/i', '', $html);
+
+        // Sanitize style attributes - allow safe CSS properties only
+        $html = preg_replace_callback('/style\s*=\s*["\']([^"\']*)["\']/i', function ($matches) {
+            $style = $matches[1];
+            // Allow only safe CSS properties
+            $allowedProperties = [
+                'color',
+                'background-color',
+                'font-size',
+                'font-weight',
+                'font-style',
+                'text-decoration',
+                'text-align',
+                'margin',
+                'padding',
+                'border',
+                'width',
+                'height',
+                'display',
+                'float',
+                'clear',
+                'position',
+                'top',
+                'left',
+                'right',
+                'bottom',
+                'line-height',
+                'letter-spacing',
+                'text-indent',
+                'vertical-align',
+                'white-space',
+                'word-wrap'
+            ];
+
+            $safeStyles = [];
+            $properties = explode(';', $style);
+            foreach ($properties as $property) {
+                $property = trim($property);
+                if (empty($property)) continue;
+
+                list($name, $value) = explode(':', $property, 2);
+                $name = trim(strtolower($name));
+                $value = trim($value);
+
+                // Check if property is allowed and value doesn't contain dangerous content
+                if (in_array($name, $allowedProperties) && !preg_match('/(javascript|vbscript|expression|url\s*\()/i', $value)) {
+                    $safeStyles[] = $name . ':' . $value;
+                }
+            }
+
+            return 'style="' . implode('; ', $safeStyles) . '"';
+        }, $html);
 
         // Additional XSS prevention
         $html = preg_replace('/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/mi', '', $html);
         $html = preg_replace('/<iframe\b[^<]*(?:(?!<\/iframe>)<[^<]*)*<\/iframe>/mi', '', $html);
+        $html = preg_replace('/<object\b[^<]*(?:(?!<\/object>)<[^<]*)*<\/object>/mi', '', $html);
+        $html = preg_replace('/<embed\b[^<]*(?:(?!<\/embed>)<[^<]*)*<\/embed>/mi', '', $html);
 
         return $html;
     }
