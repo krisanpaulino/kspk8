8c57467f4bd4ced22cde714c7c1f7f8c34959ab3
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2026-03-12 15:12:30 +0800

    Update cerita_edit.php

--- app/Views/admin/cerita_edit.php
+++ app/Views/admin/cerita_edit.php
@@ -9,7 +9,7 @@
                 <input type="hidden" name="cerita_id" value="<?= $cerita->cerita_id ?>">
                 <div class="mb-3">
                     <label for="cerita_nama" class="form-label"><span class="text-danger">*</span> Nama Alumni</label>
-                    <input type="text" class="form-control <?= (isset(session('errors')['cerita_nama'])) ? 'is-invalid' : '' ?>" id="cerita_nama" name="cerita_nama" value="<?= old('cerita_nama') ?>" required>
+                    <input type="text" class="form-control <?= (isset(session('errors')['cerita_nama'])) ? 'is-invalid' : '' ?>" id="cerita_nama" name="cerita_nama" value="<?= old('cerita_nama', $cerita->cerita_nama) ?>" required>
                 </div>
                 <div class="mb-3">
                     <label for="cerita_judul" class="form-label">Judul Cerita</label>
