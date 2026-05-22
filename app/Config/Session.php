1ef414b888d8fa9fdd6e34a34ee7e0551883515b
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2026-05-22 11:45:24 +0800

    session handler

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -21,7 +21,7 @@ class Session extends BaseConfig
      *
      * @var class-string<BaseHandler>
      */
-    public string $driver = FileHandler::class;
+    public string $driver = \CodeIgniter\Session\Handlers\DatabaseHandler::class;
 
     /**
      * --------------------------------------------------------------------------
@@ -30,7 +30,7 @@ class Session extends BaseConfig
      *
      * The session cookie name, must contain only [0-9a-z_-] characters
      */
-    public string $cookieName = 'ci_session';
+    public string $cookieName = 'kristianpaulino_ganteng';
 
     /**
      * --------------------------------------------------------------------------
@@ -57,7 +57,7 @@ class Session extends BaseConfig
      *
      * IMPORTANT: You are REQUIRED to set a valid save path!
      */
-    public string $savePath = WRITEPATH . 'session';
+    public string $savePath = 'ci_sessions';
 
     /**
      * --------------------------------------------------------------------------
---
+++ app/Database/Migrations/2026-05-22-034350_CreateSessionTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateSessionTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 128,
+                'null'       => false,
+            ],
+            'ip_address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 45,
+                'null'       => false,
+            ],
+            'timestamp' => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'default'    => 0,
+                'null'       => false,
+            ],
+            'data' => [
+                'type' => 'BLOB',
+                'null' => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); // Sets ID as Primary Key
+        $this->forge->addKey('timestamp'); // Sets index for session garbage collection
+        $this->forge->createTable('ci_sessions', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('ci_sessions', true);
+    }
+}
